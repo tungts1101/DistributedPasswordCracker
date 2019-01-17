57a040dc73bc12ae2cf256487f35e4bc006fdef6
tung <tungts1101@gmail.com>
2019-01-17 10:21:35 +0700

    change range of password

--- include/create_hash.c
+++ include/create_hash.c
@@ -2,18 +2,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include "config.h"
+#include <stdlib.h>
 
 void main(int argc, char **argv) {
 	if(argc < 2)
 		return;
 
-	if(strlen(argv[1]) > PW_LENGTH) {
-		return;
-	}
-
-	char password[PW_LENGTH];
+	char SALT[2] = "aa";
+	char *password = malloc(100);
+	memset(password, 0, 100);
 	strcpy(password, argv[1]);
+	password[strlen(password) - 1] = '\0';
 
 	printf("password = %s\n", password);
 	printf("%s\n", crypt(password, SALT));
